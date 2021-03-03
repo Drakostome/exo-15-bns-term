c976b7185f3f3793c006c0d648a8c8ff199f1a25
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-03-03 18:36:39 +0100

    Update test_exercice1.py

--- tests/test_exercice1.py
+++ tests/test_exercice1.py
@@ -1,6 +1,6 @@
 from exercices.exercice1 import *
 
-def test_tableau_minmax():
+def test_rechercheMinMax():
     tableau = [0, 1, 4, 2, -2, 9, 3, 1, 7, 1] 
     resultat = rechercheMinMax(tableau) 
     assert resultat  == {'min': -2, 'max': 9} 
