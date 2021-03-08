7c152b85580f0bfc9dbaa098a23cc4e760748f30
Drakostome <70886069+Drakostome@users.noreply.github.com>
2021-03-08 12:02:41 +0100

    Update exercice1.py

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,3 +1,13 @@
 def rechercheMinMax(tab):
-    ...
-    return ...
+    if tab == [] :
+        return {'min' : None, 'max ': None}
+    else :
+        for i in tab :
+            max = tab[0]
+            if i >= max :
+                max = i
+        for j in tab:
+            min = tab[0]
+            if j<= min :
+                min = j
+    return {'min' : min,' max ': max}
